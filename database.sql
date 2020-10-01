e8ca81b0f19fe999c92d1cd186f76563c20f04aa
Taufik Algi Fahri <taufik.algi99@gmail.com>
2020-10-01 22:44:31 +0700

    db: create receipts table

---
+++ database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bintex;
+
+CREATE TABLE receipts (
+  receipt_number VARCHAR(255) PRIMARY KEY,
+  origin VARCHAR(255) NOT NULL,
+  destination VARCHAR(255) NOT NULL,
+  shipment_date DATETIME NOT NULL,
+  consignee VARCHAR(255) NOT NULL,
+  shipper VARCHAR(255) NOT NULL,
+  drop_point VARCHAR(255)[],
+  drop_time VARCHAR(255)[]
+);
