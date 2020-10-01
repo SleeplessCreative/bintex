c22a4f5c3ef91374ebf3aea392391c0d46991118
Taufik Algi Fahri <taufik.algi99@gmail.com>
2020-10-01 22:49:38 +0700

    db: fix shipment_date data type and move the file to server

--- server/database.sql
+++ server/database.sql
@@ -4,7 +4,7 @@ CREATE TABLE receipts (
   receipt_number VARCHAR(255) PRIMARY KEY,
   origin VARCHAR(255) NOT NULL,
   destination VARCHAR(255) NOT NULL,
-  shipment_date DATETIME NOT NULL,
+  shipment_date TIMESTAMP NOT NULL,
   consignee VARCHAR(255) NOT NULL,
   shipper VARCHAR(255) NOT NULL,
   drop_point VARCHAR(255)[],
