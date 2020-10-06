c7109236fe1d37d11ea587a62115aedb77ee8e54
Muhammad Erald Setyaki Siregar <erald.muhammad@gmail.com>
2020-10-06 23:15:16 +0700

    :rocket: feat: Add register route

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const loginController = require("../controllers/authentication/login");
+const registerController = require("../controllers/authentication/register");
 
 const router = express.Router();
 
 router.post("/login", loginController.login);
+router.post("/register", registerController.login);
 
 module.exports = router;
