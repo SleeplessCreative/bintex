0d2f095ea73b0645116c655778b28efd48501ad7
Taufik Algi Fahri <taufik.algi99@gmail.com>
2020-10-08 20:00:11 +0700

    :hammer: perf: Update how the controller return json

--- server/controllers/deliveryCost.js
+++ server/controllers/deliveryCost.js
@@ -1,6 +1,9 @@
 const axios = require("axios");
 const request = require("request");
 
+const { pool } = require("../db/dev/pool");
+const error = require("./error");
+
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -22,28 +25,64 @@ exports.getCityId = async (req, res, next) => {
 };
 
 exports.getCost = async (req, res, next) => {
-  originId = req.body.originId;
-  destinationId = req.body.destinationId;
+  origin = req.body.origin;
+  destination = req.body.destination;
   weight = req.body.weight;
 
-  const options = {
-    method: "POST",
-    url: "https://api.rajaongkir.com/starter/cost",
-    headers: {
-      key: process.env.API_ONGKIR,
-      "content-type": "application/x-www-form-urlencoded",
-    },
-    form: {
-      origin: originId,
-      destination: destinationId,
-      weight: parseInt(weight),
-      courier: "jne",
-    },
-  };
+  origin = origin.replace(/(^\w{1})|(\s{1}\w{1})/g, (match) =>
+    match.toUpperCase()
+  );
+  destination = destination.replace(/(^\w{1})|(\s{1}\w{1})/g, (match) =>
+    match.toUpperCase()
+  );
+  console.log(origin);
 
   try {
+    const originIdQuery = await pool.query(
+      `SELECT city_id FROM cities where city = '${origin}'`
+    );
+
+    const destinationIdQuery = await pool.query(
+      `SELECT city_id FROM cities where city = '${destination}'`
+    );
+
+    let originId;
+    let destinationId;
+
+    // console.log(originIdQuery.rowCount);
+    if (originIdQuery.rows) {
+      originId = originIdQuery.rows[0].city_id;
+    } else {
+      error.errorFunc(404, "Origin not found");
+    }
+
+    if (destinationIdQuery.rows) {
+      destinationId = destinationIdQuery.rows[0].city_id;
+    } else {
+      error.errorFunc(404, "Destination not found");
+    }
+
+    // console.log(originIdQuery, destinationIdQuery);
+
+    const options = {
+      method: "POST",
+      url: "https://api.rajaongkir.com/starter/cost",
+      headers: {
+        key: process.env.API_ONGKIR,
+        "content-type": "application/x-www-form-urlencoded",
+      },
+      form: {
+        origin: originId,
+        destination: destinationId,
+        weight: parseInt(weight),
+        courier: "jne",
+      },
+    };
     await request(options, function (error, response, body) {
-      return res.status(201).send(body);
+      // console.log(JSON.parse(body));
+      const ongkir = JSON.parse(body).rajaongkir.results[0].costs[1].cost[0];
+      // console.log(ongkir);
+      return res.status(200).json(ongkir);
     });
   } catch (error) {
     throw new Error(error);
