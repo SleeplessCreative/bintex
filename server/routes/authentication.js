3d244a759ffd7eaa14813a68ec4f518d79f74599
Muhammad Erald Setyaki Siregar <erald.muhammad@gmail.com>
2020-10-08 17:28:14 +0700

    :hammer: Fix route for register

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -6,6 +6,6 @@ const registerController = require("../controllers/authentication/register");
 const router = express.Router();
 
 router.post("/login", loginController.login);
-router.post("/register", registerController.login);
+router.post("/register", registerController.register);
 
 module.exports = router;
