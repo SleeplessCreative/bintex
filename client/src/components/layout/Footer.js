3ec6476c5148671352a09b174c63af84f46d24b1
arridhow <arridhow@gmail.com>
2020-10-08 19:39:19 +0700

    :rocket: [Add] footer

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import LogoIcon from '../../svg/LogoIcon';
+import Phone from '../../svg/Phone';
 
 const Footer = () => (
-  <footer className="">
-    <div className = "text-center">
-      <h1 className = "text-4xl">It's crazy how fast time flies and how things progress.</h1>
-      <h1 className = "text-2xl"> Nathan Chen </h1>
+  <footer className="px-32 py-6">
+    <div className="flex flex-warp justify-center">
+      <div className="w-full lg:w-1/3">
+        <h2 className="text-lg font-semibold">BINTEX (c) 2020</h2>
+      </div>
+      <div className="w-full lg:w-1/3">
+        <h2 className="text-lg font-semibold">Contacts</h2>
+        <ul className="mt-4 leading-loose">
+          <li>
+            <a href="">08238777545</a>
+          </li>
+          <li>
+            <a href="">customer.care@bintex.id</a>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full lg:w-1/3">
+        <h2 className="text-lg font-semibold">Download</h2>
+        <ul className="mt-4 leading-loose">
+          <li>
+            <a href="">Android</a>
+          </li>
+          <li>
+            <a href="">IOS</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </footer>
 );
