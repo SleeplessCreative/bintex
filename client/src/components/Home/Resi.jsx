67a88b127d21937cbb098d60ec1e80fcdf2a4b2a
Taufik Algi Fahri <taufik.algi99@gmail.com>
2020-10-09 00:56:11 +0700

    :hammer: fix: Fix api url

--- client/src/components/Home/Resi.jsx
+++ client/src/components/Home/Resi.jsx
@@ -24,7 +24,7 @@ const Resi = () => {
   const handleSubmit = async e => {
     e.preventDefault();
     await axios
-      .post('http://localhost:8000/api/invoice/get-receipts', {
+      .post('http://api.bintex.id/api/invoice/get-receipts', {
         receipt: noResi.receipt
       })
       .then(res => {
