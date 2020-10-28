26b813ae8f12ab4c67fe2ff8b626dbd06741c6a3
Taufik Algi Fahri <taufik.algi99@gmail.com>
2020-10-28 23:10:14 +0700

    :construction: feat: Create feature to search delivery cost

--- client/src/components/Home/Ongkir.jsx
+++ client/src/components/Home/Ongkir.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import Button from '../../components/Button';
 
 const Ongkir = () => {
+  const [data, setData] = useState({
+    origin: '',
+    destination: '',
+    weight: '1'
+  });
+
+  const [cost, setCost] = useState({
+    requested: false,
+    cost: ''
+  });
+
+  // console.log(data.origin, data.destination, data.berat);
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    console.log('test', data.origin, data.destination, data.weight);
+    await axios
+      .get('http://localhost:5000/api/delivery/cost', {
+        origin: data.origin,
+        destination: data.destination,
+        weight: data.weight
+      })
+      .then(res => {
+        const data = res.value;
+        console.log(data);
+        setCost({
+          requested: true,
+          cost: data
+        });
+        // console.log(data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log('ahaha', cost.requested);
+  };
+
+  console.log('ahaha', cost.requested);
+  console.log(data.origin, data.destination, data.weight);
+
   return (
     <section id="cekongkir" className="py-20 lg:py-40 bg-white">
       <h1 className="text-4xl text-center">Cek Ongkir</h1>
@@ -17,6 +58,7 @@ const Ongkir = () => {
               required=""
               placeholder="Asal"
               aria-label=""
+              onChange={e => setData({ ...data, origin: e.target.value })}
             />
           </div>
           <div className="mt-2">
@@ -28,6 +70,7 @@ const Ongkir = () => {
               required=""
               placeholder="Tujuan"
               aria-label=""
+              onChange={e => setData({ ...data, destination: e.target.value })}
             />
           </div>
           <div className="mt-2">
@@ -39,9 +82,10 @@ const Ongkir = () => {
               required=""
               placeholder="Berat (Kg)"
               aria-label=""
+              onChange={e => setData({ ...data, weight: e.target.value })}
             />
           </div>
-          <Button className="mt-2 w-full" size="lg">
+          <Button className="mt-2 w-full" size="lg" func={handleSubmit}>
             Cek Ongkir
           </Button>
         </form>
