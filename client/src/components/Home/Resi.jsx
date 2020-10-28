1c515440444a0219ccbb16346033ba352c3f1e7c
Taufik Algi Fahri <taufik.algi99@gmail.com>
2020-10-28 23:09:23 +0700

    :art: refactor: Move tracking to new component

--- client/src/components/Home/Resi.jsx
+++ client/src/components/Home/Resi.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 
+import Tracking from './Tracking';
+
 import Search from '../../svg/Search';
 import Arrow from '../../svg/Arrow';
 
@@ -24,7 +26,7 @@ const Resi = () => {
   const handleSubmit = async e => {
     e.preventDefault();
     await axios
-      .post('https://api.bintex.id/api/invoice/get-receipts', {
+      .post('https://staging.bintex.id/api/invoice/get-receipts', {
         receipt: noResi.receipt
       })
       .then(res => {
@@ -53,7 +55,7 @@ const Resi = () => {
     setResi({ ...Resi, receipt: false });
   };
 
-  console.log(resi.receipt);
+  // console.log(resi.receipt);
   return (
     <>
       <section id="cekresi" className="py-20 lg:py-40 bg-primary-light">
@@ -76,41 +78,7 @@ const Resi = () => {
         </div>
         {resi.receipt ? (
           <div>
-            <div className="container m-auto justify-center bg-primary-default pt-5">
-              <h1 className="text-6x1 text-center pb-4">Informasi</h1>
-              <table className="justify-center m-auto ">
-                <tbody>
-                  <tr className="bg-primary-light">
-                    <td className="border px-4 py-2 w-3/5">No Resi</td>
-                    <td className="border px-4 py-2">{resi.receiptNumber}</td>
-                  </tr>
-                  <tr className="bg-gray-100">
-                    <td className="border px-4 py-2">Status</td>
-                    <td className="border px-4 py-2">{resi.status}</td>
-                  </tr>
-                  <tr className="bg-primary-light">
-                    <td className="border px-4 py-2">Tanggal Pengiriman</td>
-                    <td className="border px-4 py-2">{resi.shipmentDate}</td>
-                  </tr>
-                  <tr className="bg-gray-100">
-                    <td className="border px-4 py-2">Pengirim</td>
-                    <td className="border px-4 py-2">{resi.shipper}</td>
-                  </tr>
-                  <tr className="bg-primary-light">
-                    <td className="border px-4 py-2">Asal</td>
-                    <td className="border px-4 py-2">{resi.origin}</td>
-                  </tr>
-                  <tr className="bg-gray-100">
-                    <td className="border px-4 py-2">Penerima</td>
-                    <td className="border px-4 py-2">{resi.consignee}</td>
-                  </tr>
-                  <tr className="bg-primary-light">
-                    <td className="border px-4 py-2">Tujuan</td>
-                    <td className="border px-4 py-2">{resi.destination}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+            <Tracking resi={resi} />
             <div className="bg-accent-light w-full text-center mt-10">
               <button className="w-16" type="submit" onClick={handleHideSubmit}>
                 <Arrow />
