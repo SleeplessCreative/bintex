e8d9bd338717846a4bd68fd03febfd38b47ab485
Taufik Algi Fahri <taufik.algi99@gmail.com>
2020-12-03 02:09:06 +0700

    build: Update script and config.js

--- server/config/config.js
+++ server/config/config.js
@@ -9,18 +9,18 @@ module.exports = {
     host: "127.0.0.1",
     dialect: "postgres",
   },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
+  staging: {
+    username: process.env.STG_USERNAME,
+    password: process.env.STG_PASS,
+    database: process.env.STG_DATABASE,
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "postgres",
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
+    username: process.env.PROD_USERNAME,
+    password: process.env.PROD_PASS,
+    database: process.env.PROD_DATABASE,
     host: "127.0.0.1",
-    dialect: "mysql",
+    dialect: "postgres",
   },
 };
