56e7f1d9d844a7f1bb638b225860be3a2efe119e
Taufik Algi Fahri <taufik.algi99@gmail.com>
2020-12-06 23:54:07 +0700

    feat: Add driver related controller

--- server/routes/driver.js
+++ server/routes/driver.js
@@ -5,7 +5,7 @@ const driverController = require("../controllers/driver/driver");
 const router = express.Router();
 
 router.post("/deliver", driverController.deliver);
-router.get("/get-driver-histories", driverController.getHistory);
+router.post("/get-driver-histories", driverController.getHistory);
 router.post("/regist-driver", driverController.registerDriver);
 router.get("/get-driver", driverController.getAllDriver);
 router.get("/get-user-driver", driverController.getAllUserWithRoleDriver);
