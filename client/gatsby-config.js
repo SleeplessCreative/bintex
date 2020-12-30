9202a5f8db2343a38b24a5bbbf077896217a32c0
arridhow <arridhow@gmail.com>
2020-12-31 02:59:01 +0700

    :pencil: SEO

--- client/gatsby-config.js
+++ client/gatsby-config.js
@@ -1,4 +1,10 @@
 module.exports = {
+  siteMetadata: {
+    title: `Bintex ID`,
+    description: `A new delivery service that will take good control of your delivery process in order to make sure that your packages are delivered to you on time.`,
+    author: `Bintang Express Indonesia`,
+    url: `https://bintex.id`
+  },
   plugins: [
     {
       resolve: `gatsby-theme-codebushi`,
